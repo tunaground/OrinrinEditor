03c46b8f5360ae7347a2bd67d8635bac215cff25
NNN1590 <nnn1590@nnn1590.org>
2022-05-03 00:42:16 +0900

    src/{DocPreview,TraceCtrl}.cpp: スタブ化
    今の所ATLが利用できないのと、ImgCtlがプロプラ？なのでスタブ化
    プレビューとトレス機能が使えなくなる
    
    このコミットでリリースモードでビルドできるようになってるはず…

--- src/DocPreview.cpp
+++ src/DocPreview.cpp
@@ -44,96 +44,6 @@ ttp://www.microsoft.com/download/en/details.aspx?displaylang=en&id=11800
 
 */
 
-#pragma warning( disable : 4995 )	//	ATL内のイケナイ函数警告を中止！
-
-#include <atlbase.h>
-
-#ifndef _ATL
-#error IEコンポつかうにはATLが要る。
-#endif
-
-//	DDKのATL使うなら必要なようだ
-#pragma comment(lib, "atlthunk.lib")
-
-
-//	ATLを使用するために必要なグローバル変数
-//	この変数名は、必ず、この名前にすること。
-extern CComModule	_Module;
-
-#include <atlcom.h>
-#include <atlhost.h>
-
-#pragma warning( default : 4995 )	//	警告復帰
-
-//#if _MSC_VER >=1500 // VC2008(VC9.0)以降
-
-//	DDKなら、AtlAxWin80 っぽい
-
-
-#if _ATL_VER == 0x0800
-#define ATL_AX_WIN	TEXT("AtlAxWin80")
-#else
-#error ATLバージョンに合わせて定義する必要がある
-#endif
-
-//	ATLAXWIN_CLASS	定義されてるけどTEXTになってない
-
-CComModule	_Module;
-
-//	なんか要るみたい
-BEGIN_OBJECT_MAP(ObjectMap)
-END_OBJECT_MAP( )
-
-//-------------------------------------------------------------------------------------------------
-
-#define REDRAW_ATSCROLL
-
-#define DOC_PREVIEW_CLASS	TEXT("PREVIEW_CLASS")	//!<	プレビュー窓のクラス名
-
-#define PVW_WIDTH	 820	//!<	デフォルト画面幅
-#define PVW_HEIGHT	 480	//!<	デフォルト画面高さ
-//-------------------------------------------------------------------------------------------------
-
-static CComQIPtr<IHTMLDocument2>	gpDocument2;	//!<	ＩＥコンポーネントのなにか
-static CComQIPtr<IWebBrowser2>		gpWebBrowser2;	//!<	ＩＥコンポーネントのナニカ
-static  HWND	ghIEwnd;							//!<	ＩＥコンポーネントのハンドル
-
-
-
-static  HWND	ghPrevWnd;			//!<	このウインドウのハンドル
-static HINSTANCE	ghInst;			//!<	現在のインターフェイス
-
-static  HWND	ghToolWnd;			//!<	ツールバー
-static HIMAGELIST ghPrevwImgLst;	//!<	ツールバーアイコンのイメージリスト
-
-static INT		giViewMode;			//!<	表示状態：０〜単体頁　−１全頁
-
-extern  HWND	ghPrntWnd;				//	親ウインドウハンドル
-extern list<ONEFILE>	gltMultiFiles;	//	複数ファイル保持
-extern FILES_ITR	gitFileIt;			//	今見てるファイルの本体
-extern INT		gixFocusPage;			//	注目中のページ・とりあえず０・０インデックス
-//-------------------------------------------------------------------------------------------------
-
-#define TB_ITEMS	1
-static  TBBUTTON	gstTBInfo[] = {
-	{ 0, IDM_PVW_ALLVW, TBSTATE_ENABLED, TBSTYLE_BUTTON | TBSTYLE_AUTOSIZE | TBSTYLE_CHECK, {0, 0}, 0, 0 }
-};	//	
-
-
-#define PREV_HEADER_FILE	TEXT("Preview.htm")
-#define SEPARATE_TAG	("[SEPARATE]")
-
-static CONST CHAR	gcacWeek[7][4] = { ("日"), ("月"), ("火"), ("水"), ("木"), ("金"), ("土") };
-
-static  CHAR	gacResHeaderFmt[] = { "<dt>%d 名前：名無しの妖精さん[sage] 投稿日：%d/%02d/%02d(%s) %02d:%02d:%02d ID:OrinEdit99</dt> <dd>" };	//	
-static  CHAR	gacResFooterFmt[] = { "<br></dd>\r\n" };
-
-
-static LPSTR	gpcHtmlHdr;
-static LPSTR	gpcHtmlFtr;
-//-------------------------------------------------------------------------------------------------
-
-
 LRESULT	CALLBACK PreviewWndProc( HWND, UINT, WPARAM, LPARAM );	//!<	
 VOID	Pvw_OnCommand( HWND , INT, HWND, UINT );	//!<	WM_COMMAND の処理
 VOID	Pvw_OnSize( HWND , UINT, INT, INT );		//!<	
@@ -151,62 +61,7 @@ HRESULT	PreviewPageWrite( INT );	//!<
 	@param[in]	hParentWnd	親ウインドウのハンドル
 	@return	なし
 */
-VOID PreviewInitialise( HINSTANCE hInstance, HWND hParentWnd )
-{
-	WNDCLASSEX	wcex;
-	GUID	guid;
-
-	HBITMAP	hImg, hMsq;
-
-	if( hInstance )
-	{
-		ghInst = hInstance;
-
-		_Module.Init( ObjectMap, hInstance, &guid );
-
-		//	専用のウインドウクラス作成
-		ZeroMemory( &wcex, sizeof(WNDCLASSEX) );
-		wcex.cbSize			= sizeof(WNDCLASSEX);
-		wcex.style			= CS_HREDRAW | CS_VREDRAW;
-		wcex.lpfnWndProc	= PreviewWndProc;
-		wcex.cbClsExtra		= 0;
-		wcex.cbWndExtra		= 0;
-		wcex.hInstance		= hInstance;
-		wcex.hIcon			= NULL;
-		wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-		wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-		wcex.lpszMenuName	= NULL;
-		wcex.lpszClassName	= DOC_PREVIEW_CLASS;
-		wcex.hIconSm		= NULL;
-
-		RegisterClassEx( &wcex );
-
-		ghPrevWnd = NULL;
-
-		PreviewHeaderGet(  );
-
-		CoInitialize( NULL );
-
-		giViewMode = 0;
-
-		ghPrevwImgLst = ImageList_Create( 16, 16, ILC_COLOR24 | ILC_MASK, 1, 1 );
-		hImg = LoadBitmap( ghInst, MAKEINTRESOURCE( (IDBMP_PREVIEW_ALL) ) );
-		hMsq = LoadBitmap( ghInst, MAKEINTRESOURCE( (IDBMQ_PREVIEW_ALL) ) );
-		ImageList_Add( ghPrevwImgLst , hImg, hMsq );	//	イメージリストにイメージを追加
-		DeleteBitmap( hImg );	DeleteBitmap( hMsq );
-	}
-	else
-	{
-		//	もし開けたままだったら綺麗に閉じないとメモリリーク
-		if( ghPrevWnd ){	SendMessage( ghPrevWnd, WM_CLOSE, 0, 0 );	}
-
-		CoUninitialize( );
-
-		FREE(gpcHtmlHdr);
-
-		ImageList_Destroy( ghPrevwImgLst );
-	}
-
+VOID PreviewInitialise( HINSTANCE hInstance, HWND hParentWnd ) {
 	return;
 }
 //-------------------------------------------------------------------------------------------------
@@ -214,46 +69,8 @@ VOID PreviewInitialise( HINSTANCE hInstance, HWND hParentWnd )
 /*!
 	テンプレディレクトリに入ってるHeaderFooterファイルの中身をゲットする
 */
-HRESULT PreviewHeaderGet( VOID )
-{
-	TCHAR	atPrevFile[MAX_PATH];
-
-	HANDLE	hFile;
-	DWORD	readed;
-
-	INT		iByteSize;
-	LPSTR	pcText, pcNext;
-
-
-	gpcHtmlHdr = NULL;
-	gpcHtmlFtr = NULL;
-
-	//	ヘッダ・フッタを確保
-	StringCchCopy( atPrevFile, MAX_PATH, ExePathGet() );
-	PathAppend( atPrevFile, TEMPLATE_DIR );
-	PathAppend( atPrevFile, PREV_HEADER_FILE );
-
-	hFile = CreateFile( atPrevFile, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL );
-	if( INVALID_HANDLE_VALUE == hFile ){	return E_HANDLE;	}
-
-	iByteSize = GetFileSize( hFile, NULL );
-	pcText = (LPSTR)malloc( iByteSize + 2 );
-	ZeroMemory( pcText, iByteSize + 2 );
-
-	SetFilePointer( hFile, 0, NULL, FILE_BEGIN );
-	ReadFile( hFile, pcText, iByteSize, &readed, NULL );
-	CloseHandle( hFile );	//	内容全部取り込んだから開放
-
-	gpcHtmlHdr = pcText;	//	HEADER部分・freeにもつかう
-
-	pcNext = StrStrA( pcText, SEPARATE_TAG );
-	pcNext[0] = NULL;	//	HEADERとFOOTERの境界にヌルターミネータ
-	pcNext++;
-	gpcHtmlFtr = NextLineA( pcNext );	//	次の行からが本番
-
-
-
-	return S_OK;
+HRESULT PreviewHeaderGet( VOID ) {
+	return E_FAIL;
 }
 //-------------------------------------------------------------------------------------------------
 
@@ -263,163 +80,8 @@ HRESULT PreviewHeaderGet( VOID )
 	@param[in]	bForeg		非０なら再プレビューのときにフォアグランドにする・０ならしない・プレビュー開いてないなら何もしない
 	@return	HRESULT	終了状態コード
 */
-HRESULT PreviewVisibalise( INT iNowPage, BOOLEAN bForeg )
-{
-	HWND	hWnd;
-	TCHAR	atBuffer[MAX_STRING];
-//	UINT	iIndex;
-	RECT	rect;
-	RECT	tbRect;
-
-	HRESULT	hRslt;
-
-	CComPtr<IUnknown>	comPunkIE;	
-	CComPtr<IDispatch>	pDispatch;
-	CComVariant	vEmpty;
-	CComVariant	vUrl( TEXT("about:blank") );
-
-#ifdef REDRAW_ATSCROLL
-	LONG	height, offhei, scrtop;
-//	CComQIPtr<IHTMLWindow2>		pWindow2;
-	CComQIPtr<IHTMLElement>		pElement;
-	CComQIPtr<IHTMLElement2>	pElement2;
-#endif
-
-	if( ghPrevWnd )	//	已にPreview窓有ったら
-	{
-	//	SendMessage( ghToolWnd, TB_CHECKBUTTON, IDM_PVW_ALLVW, FALSE );
-
-#pragma message ("全プレ書換時に、スクロールバーの位置覚えておいて、そこまでScrollさせる？")
-
-#ifdef REDRAW_ATSCROLL
-		gpWebBrowser2->get_Height( &height );	//	多分コンポーネントの高さ
-
-		gpDocument2->get_body( &pElement );
-		pElement.QueryInterface( &pElement2 );
-		pElement.Release(  );
-
-		pElement2->get_scrollHeight( &offhei );	//	全体の高さ
-		pElement2->get_scrollTop( &scrtop );	//	表示位置のスクロール量
-		pElement2.Release(  );
-
-#endif
-
-		//	内容書き換え
-		if( 0 > giViewMode ){	PreviewPageWrite(  -1 );	}
-		else{	PreviewPageWrite( iNowPage );	}
-
-		gpWebBrowser2->Refresh(  );
-
-#ifdef REDRAW_ATSCROLL
-		gpDocument2->get_body( &pElement );
-		pElement.QueryInterface( &pElement2 );
-		pElement.Release(  );
-
-		pElement2->get_scrollHeight( &offhei );	//	全体の高さ
-
-		pElement2->put_scrollTop(  scrtop );	//	表示位置のスクロール量
-		pElement2.Release(  );
-#endif
-
-		InvalidateRect( ghPrevWnd, NULL, TRUE );
-
-		if( bForeg )	SetForegroundWindow( ghPrevWnd );
-
-		return S_FALSE;
-	}
-
-	//	プレビュー開いてないときに、非フォアグランドなら何もしない
-	if( !(bForeg) ){	return  E_ABORT;	}
-
-
-	InitWindowPos( INIT_LOAD, WDP_PREVIEW, &rect );
-	if( 0 >= rect.right || 0 >= rect.bottom )	//	幅高さが０はデータ無し
-	{
-		hWnd = GetDesktopWindow( );
-		GetWindowRect( hWnd, &rect );
-		rect.left   = ( rect.right  - PVW_WIDTH ) / 2;
-		rect.top    = ( rect.bottom - PVW_HEIGHT ) / 2;
-		rect.right  = PVW_WIDTH;
-		rect.bottom = PVW_HEIGHT;
-		InitWindowPos( INIT_SAVE , WDP_PREVIEW, &rect );	//	起動時保存
-	}
-
-
-	ghPrevWnd = CreateWindowEx( WS_EX_TOOLWINDOW, DOC_PREVIEW_CLASS, TEXT("IEコンポーネントによるプレビュー"),
-		WS_POPUP | WS_THICKFRAME | WS_CAPTION | WS_VISIBLE | WS_SYSMENU,
-		rect.left, rect.top, rect.right, rect.bottom, NULL, NULL, ghInst, NULL );
-
-	//ツールバー作る
-	ghToolWnd = CreateWindowEx( 0, TOOLBARCLASSNAME, TEXT("toolbar"),
-		WS_CHILD | WS_VISIBLE | TBSTYLE_FLAT | TBSTYLE_LIST | TBSTYLE_TOOLTIPS | CCS_NODIVIDER,
-		0, 0, 0, 0, ghPrevWnd, (HMENU)IDW_PVW_TOOL_BAR, ghInst, NULL );
-
-	//	自動ツールチップスタイルを追加
-	SendMessage( ghToolWnd, TB_SETEXTENDEDSTYLE, 0, TBSTYLE_EX_MIXEDBUTTONS );
-
-	SendMessage( ghToolWnd, TB_SETIMAGELIST, 0, (LPARAM)ghPrevwImgLst );
-
-	SendMessage( ghToolWnd, TB_SETBUTTONSIZE, 0, MAKELPARAM(16,16) );
-
-	SendMessage( ghToolWnd, TB_BUTTONSTRUCTSIZE, (WPARAM)sizeof(TBBUTTON), 0 );
-	//	ツールチップ文字列を設定・ボタンテキストがツールチップになる
-	StringCchCopy( atBuffer, MAX_STRING, TEXT("全プレビュースタイル") );	gstTBInfo[0].iString = SendMessage( ghToolWnd, TB_ADDSTRING, 0, (LPARAM)atBuffer );
-
-	SendMessage( ghToolWnd , TB_ADDBUTTONS, (WPARAM)TB_ITEMS, (LPARAM)&gstTBInfo );	//	ツールバーにボタンを挿入
-
-	SendMessage( ghToolWnd , TB_AUTOSIZE, 0, 0 );	//	ボタンのサイズに合わせてツールバーをリサイズ
-	InvalidateRect( ghToolWnd , NULL, TRUE );		//	クライアント全体を再描画する命令
-
-	//	ツールバーサブクラス化が必要
-
-	GetWindowRect( ghToolWnd, &tbRect );
-	tbRect.right  -= tbRect.left;
-	tbRect.bottom -= tbRect.top;
-	tbRect.left = 0;
-	tbRect.top  = 0;
-
-	GetClientRect( ghPrevWnd, &rect );
-	rect.top     = tbRect.bottom;
-	rect.bottom -= tbRect.bottom;
-
-	AtlAxWinInit(  );
-
-	ghIEwnd = CreateWindowEx( 0, ATL_AX_WIN, TEXT("Shell.Explorer.2"),
-		WS_CHILD | WS_VISIBLE, rect.left, rect.top, rect.right, rect.bottom,
-		ghPrevWnd, (HMENU)IDW_PVW_VIEW_WNDW, ghInst, NULL );
-
-	//	ActiveXコントロールのインターフェースを要求
-	if( SUCCEEDED( AtlAxGetControl( ghIEwnd, &comPunkIE ) ) )
-	{
-		gpWebBrowser2 = comPunkIE;	//	ポインタに格納
-
-		if( gpWebBrowser2 )
-		{
-			gpWebBrowser2->Navigate2( &vUrl, &vEmpty, &vEmpty, &vEmpty, &vEmpty );
-
-			while( 1 )
-			{
-				hRslt = gpWebBrowser2->get_Document( &pDispatch );
-				if( SUCCEEDED(hRslt) && pDispatch )
-				{
-					gpDocument2 = pDispatch;
-					if( gpDocument2 ){	hRslt = S_OK;	break;	}
-				}
-				Sleep(100);
-			}
-		}
-		else
-		{
-			NotifyBalloonExist( TEXT("ＩＥコンポーネントを初期化出来なかったよ・・・"), TEXT("お燐からのお知らせ"), NIIF_ERROR );
-			hRslt = E_ACCESSDENIED;
-		}
-	}
-
-	if( SUCCEEDED(hRslt)  ){	PreviewPageWrite( iNowPage );	}
-
-	UpdateWindow( ghPrevWnd );
-
-	return hRslt;
+HRESULT PreviewVisibalise( INT iNowPage, BOOLEAN bForeg ) {
+	return E_FAIL;
 }
 //-------------------------------------------------------------------------------------------------
 
@@ -433,18 +95,7 @@ HRESULT PreviewVisibalise( INT iNowPage, BOOLEAN bForeg )
 	@retval 0	メッセージ処理済み
 	@retval no0	ここでは処理せず次に回す
 */
-LRESULT CALLBACK PreviewWndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
-{
-	switch( message )
-	{
-		HANDLE_MSG( hWnd, WM_SIZE,    Pvw_OnSize );	
-		HANDLE_MSG( hWnd, WM_PAINT,   Pvw_OnPaint );	
-		HANDLE_MSG( hWnd, WM_COMMAND, Pvw_OnCommand );	
-		HANDLE_MSG( hWnd, WM_DESTROY, Pvw_OnDestroy );
-
-		default:	break;
-	}
-
+LRESULT CALLBACK PreviewWndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam ) {
 	return DefWindowProc( hWnd, message, wParam, lParam );
 }
 //-------------------------------------------------------------------------------------------------
@@ -457,22 +108,7 @@ LRESULT CALLBACK PreviewWndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM 
 	@param[in]	codeNotify	通知メッセージ	HIWORD(wParam)
 	@return		なし
 */
-VOID Pvw_OnCommand( HWND hWnd, INT id, HWND hWndCtl, UINT codeNotify )
-{
-	LRESULT	lRslt;
-
-	switch( id )
-	{
-		case IDM_PVW_ALLVW:	//	全プレON/OFF
-			lRslt = SendMessage( ghToolWnd, TB_ISBUTTONCHECKED, IDM_PVW_ALLVW, 0 );
-			if( lRslt )	PreviewPageWrite(  -1 );
-			else		PreviewPageWrite( gixFocusPage );
-			InvalidateRect( ghPrevWnd, NULL, TRUE );
-			break;
-
-		default:	break;
-	}
-
+VOID Pvw_OnCommand( HWND hWnd, INT id, HWND hWndCtl, UINT codeNotify ) {
 	return;
 }
 //-------------------------------------------------------------------------------------------------
@@ -484,29 +120,7 @@ VOID Pvw_OnCommand( HWND hWnd, INT id, HWND hWndCtl, UINT codeNotify )
 	@param[in]	cx		変更されたクライヤント幅
 	@param[in]	cy		変更されたクライヤント高さ
 */
-VOID Pvw_OnSize( HWND hWnd, UINT state, INT cx, INT cy )
-{
-	HWND	hWorkWnd;
-	RECT	tbRect, rect;
-
-	hWorkWnd = GetDlgItem( hWnd, IDW_PVW_TOOL_BAR );
-
-	MoveWindow( hWorkWnd, 0, 0, 0, 0, TRUE );	//	ツールバーは数値なくても勝手に合わせてくれる
-
-	GetWindowRect( hWorkWnd, &tbRect );
-	tbRect.right  -= tbRect.left;
-	tbRect.bottom -= tbRect.top;
-	tbRect.left = 0;
-	tbRect.top  = 0;
-
-	GetClientRect( ghPrevWnd, &rect );
-	rect.top     = tbRect.bottom;
-	rect.bottom -= tbRect.bottom;
-
-	hWorkWnd = GetDlgItem( hWnd, IDW_PVW_VIEW_WNDW );
-
-	MoveWindow( hWorkWnd, rect.left, rect.top, rect.right, rect.bottom, TRUE );
-
+VOID Pvw_OnSize( HWND hWnd, UINT state, INT cx, INT cy ) {
 	return;
 }
 //-------------------------------------------------------------------------------------------------
@@ -516,15 +130,7 @@ VOID Pvw_OnSize( HWND hWnd, UINT state, INT cx, INT cy )
 	@param[in]	hWnd	親ウインドウのハンドル
 	@return		無し
 */
-VOID Pvw_OnPaint( HWND hWnd )
-{
-	PAINTSTRUCT	ps;
-	HDC			hdc;
-
-	hdc = BeginPaint( hWnd, &ps );
-
-	EndPaint( hWnd, &ps );
-
+VOID Pvw_OnPaint( HWND hWnd ) {
 	return;
 }
 //-------------------------------------------------------------------------------------------------
@@ -534,24 +140,7 @@ VOID Pvw_OnPaint( HWND hWnd )
 	@param[in]	hWnd	親ウインドウのハンドル
 	@return		無し
 */
-VOID Pvw_OnDestroy( HWND hWnd )
-{
-	RECT	rect;
-
-	//	ウインドウ位置も記録
-	GetWindowRect( ghPrevWnd, &rect );
-	rect.right  -= rect.left;
-	rect.bottom -= rect.top;
-	InitWindowPos( INIT_SAVE , WDP_PREVIEW, &rect );	//	終了時保存
-
-	ghPrevWnd = NULL;
-
-	gpDocument2.Release( );
-
-	gpWebBrowser2.Release( );
-
-	if( hWnd )	PostMessage( ghPrntWnd, WMP_PREVIEW_CLOSE, 0, 0 );
-
+VOID Pvw_OnDestroy( HWND hWnd ) {
 	return;
 }
 //-------------------------------------------------------------------------------------------------
@@ -560,117 +149,7 @@ VOID Pvw_OnDestroy( HWND hWnd )
 	頁を表示する
 	@param[in]	iViewPage	頁番号・−１なら全頁
 */
-HRESULT PreviewPageWrite( INT iViewPage )
-{
-	HRESULT	hRslt;
-
-	VARIANT		*param;
-	SAFEARRAY	*sfArray;
-
-	UINT_PTR	szSize;
-	INT		szCont, bstrLen;
-	INT_PTR	iPage, i;
-	LPSTR	pcContent;
-
-	CHAR	acSeper[MAX_STRING];
-	BSTR	bstr;
-
-	string	asString;
-
-	SYSTEMTIME	stTime;
-
-	hRslt = S_OK;
-
-	giViewMode = iViewPage;
-
-	if( !(gpcHtmlHdr) )
-	{
-		NotifyBalloonExist( TEXT("プレビュー用テンプレートファイルが見つからないよ"), TEXT("お燐からのお知らせ"), NIIF_ERROR );
-		return E_HANDLE;
-	}
-
-	//	表示内容をＳＪＩＳでつくる
-	GetLocalTime( &stTime );
-
-	if( 0 >  iViewPage )	//	全プレ
-	{
-		asString  = string( gpcHtmlHdr );
-
-		iPage = DocNowFilePageCount(  );	//	頁数確認して
-		for( i = 0; iPage > i; i++ )
-		{
-			StringCchPrintfA( acSeper, MAX_STRING, gacResHeaderFmt, (i+1),
-				stTime.wYear, stTime.wMonth, stTime.wDay,
-				gcacWeek[stTime.wDayOfWeek],
-				stTime.wHour, stTime.wMinute, stTime.wSecond );
-
-			pcContent = DocPageTextPreviewAlloc( i, &szCont );
-
-			asString += string( acSeper );
-			asString += string( pcContent );
-			asString += string( gacResFooterFmt );
-
-			FREE(pcContent);
-		}
-
-		asString += string( gpcHtmlFtr );
-	}
-	else
-	{
-		StringCchPrintfA( acSeper, MAX_STRING, gacResHeaderFmt, (iViewPage+1),
-			stTime.wYear, stTime.wMonth, stTime.wDay,
-			gcacWeek[stTime.wDayOfWeek],
-			stTime.wHour, stTime.wMinute, stTime.wSecond );
-
-		pcContent = DocPageTextPreviewAlloc( iViewPage, &szCont );
-
-		asString  = string( gpcHtmlHdr );
-		asString += string( acSeper );
-		asString += string( pcContent );
-		asString += string( gacResFooterFmt );
-		asString += string( gpcHtmlFtr );
-
-		FREE(pcContent);
-	}
-
-	//	BSTRに必要なサイズ確認
-	szSize = asString.size( );
-	bstrLen = MultiByteToWideChar( CP_ACP, 0, asString.c_str( ), szSize, NULL, 0 );
-
-	//	バッファを確保
-	bstr = SysAllocStringLen( NULL, bstrLen );
-
-	//	BSTRにブチこむ
-	MultiByteToWideChar( CP_ACP, 0, asString.c_str( ), szSize, bstr, bstrLen );
-
-	sfArray = SafeArrayCreateVector( VT_VARIANT, 0, 1 );
-			
-	if (sfArray == NULL || gpDocument2 == NULL)
-	{
-		goto cleanup;
-	}
-
-	hRslt = SafeArrayAccessData(sfArray, (LPVOID*)&param );
-	param->vt = VT_BSTR;
-	param->bstrVal = bstr;
-	hRslt = SafeArrayUnaccessData(sfArray);
-	hRslt = gpDocument2->writeln(sfArray);
-
-	hRslt = gpDocument2->close( );
-
-cleanup:
-	if( bstr )
-	{
-		SysFreeString( bstr );
-		hRslt = SafeArrayAccessData( sfArray, (LPVOID*)&param );
-		param->vt = VT_BSTR;
-		param->bstrVal = NULL;
-		hRslt = SafeArrayUnaccessData( sfArray );
-	}
-
-	if( sfArray ){	SafeArrayDestroy( sfArray );	}
-
-	return hRslt;
+HRESULT PreviewPageWrite( INT iViewPage ) {
+	return E_FAIL;
 }
 //-------------------------------------------------------------------------------------------------
-
